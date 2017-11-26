ce8dc55c801f7d26dc868f68bde6b9b2e14f60c9
Ayanna Scoon-Jerome <ayanna.scoon@gmail.com>
2017-11-26 17:00:15 -0500

    added first setInterval function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 //-----------------------Jeopardy-like Trivia Game --------------------------------
-
+$(document).ready(function() {
 
 // --------------------Global array to hold question objects----------------------- 
 var trivia = [{
@@ -7,7 +7,7 @@ var trivia = [{
         question: "Shinto is the indigenous religious belief and practice of this country",
         answers: ["What is Butan?", "What is Japan?", "What is Pakistan?", "What is Malaysia?"],
         rightAnswer: "What is Japan?",
-        extra: "93% of Japanese practice Shintoism, though some also practice Buddhism.",
+        quesExtra: "93% of Japanese practice Shintoism, though some also practice Buddhism.",
         container: $("<div>")
     },
 
@@ -51,34 +51,59 @@ var wrongAnswers = 0;
 var rightAnswers = 0;
 var unanswered = 0;
 var rightAnsMsgs = ["You're a genius!", "Awesome job!", "You got it!", "Well done!", "Brilliant!", "Way to go!"];
-
+var randomMsg = rightAnsMsgs[Math.floor(Math.random()*rightAnsMsgs.length)];
+var gameMsgDiv = $("<div class = 'msgDiv'></div");
+var x = document.getElementById("quesAudio"); 
+var timeLeft = 30;
+var questionTimer;
 
 //-------Creating function to display a question on the screen-----------------------------------------------
-var displayQuestion = function (index) {
-    var quesHolder = trivia[index].container;
-    var quesHeader = $("<div class = 'eachQues'></div>").text(trivia[index].question);
+var displayQuestion = function () {
+    questionTimer = setInterval(timerCountdown, 1000);
+
+    var quesHolder = trivia[currentQuesNum].container;
+    var quesHeader = $("<div class = 'eachQues'></div>").text(trivia[currentQuesNum].question);
     quesHolder.empty().append(quesHeader);
 
-    for (var i = 0; i < trivia[index].answers.length; i++) {
-        var ansBtn = $("<button class = 'ansOption'>" + trivia[index].answers[i] + "</button>").attr("data-ans", trivia[index].answers[i]);
+    for (var i = 0; i < trivia[currentQuesNum].answers.length; i++) {
+        var ansBtn = $("<button class = 'ansOption'>" + trivia[currentQuesNum].answers[i] + "</button>").attr("data-ans", trivia[currentQuesNum].answers[i]);
         quesHolder.append(ansBtn);
     }
     $("#gameArea").append(quesHolder);
+    
+   };
 
-};
+var timerCountdown = function () {
+    
+    $("#timerArea").html("<h2> You have " + timeLeft + " seconds left </h2>");
+    x.play();
+    timeLeft--;
 
-displayQuestion(currentQuesNum);
+    if (timeLeft === -1) {
+        clearInterval(questionTimer);
+        console.log('TIME UP');
+        //timeUp();
+    };
+  };
+
+  displayQuestion();
 
 //-------Function to evaluate user selection------------------------------------------------------------------
 
-var evaluateGuess = function () {
-    if ($(".ansOption").attr("data-ans") === trivia[currentQuesNum].rightAnswer) { //why is this not true??
+var evaluateGuess = function (userGuess) {
+    if (userGuess.attr("data-ans") === trivia[currentQuesNum].rightAnswer) { //why is this not true??
         console.log("You chose the right Answer");
         rightAnswers++;
+        gameMsgDiv.empty().append("<h1>" + randomMsg + "</h1>");
+        displayRightAnsMsg();
+
         //display right answer message
     } else {
         console.log("You chose the wrong Answer");
         wrongAnswers++;
+        gameMsgDiv.empty().append("<h1> Sorry... that's the wrong answer :-(</h1>");
+        displayRightAnsMsg();
+
         //display wrong answer message
     }
     //currentQuesNum++; --- Still to determine the best place to execute this--- a nextQuestion fuction???
@@ -87,11 +112,27 @@ var evaluateGuess = function () {
 //-------When the user selects an answer, run the EvaluateGuess function------------------------------------------
 
 $("#gameArea").on("click", ".ansOption", function () {
+    clearInterval(questionTimer);
+    x.pause();
     console.log("user choice: " + $(this).attr("data-ans"));
     console.log("correct answer: " + trivia[currentQuesNum].rightAnswer);
-    evaluateGuess();
-   
+    evaluateGuess($(this));
 });
 
+//--------Function to advance the game to the next question-------------------------------------------------------
+var nextQuestion = function() {
+    currentQuesNum++;
+    displayQuestion();
+};
 
+//--------Function to display message when user selects the right answer-------------------------------------------
+var displayRightAnsMsg = function() {
+    gameMsgDiv.append("<h2> The correct answer is: " + trivia[currentQuesNum].rightAnswer + "</h2>");
+    gameMsgDiv.append("<h2>" + trivia[currentQuesNum].quesExtra + "</h2>");
+    // $("#gameArea").empty().append(gameMsgDiv);
+    $("#gameArea").empty().append(gameMsgDiv);
+    
+};
+
+});
 
